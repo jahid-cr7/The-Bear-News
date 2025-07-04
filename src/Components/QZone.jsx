ce44d9dd29cfcaf706e3809ece42ebf9641e6180
jahid-cr7 <jahidhasan@stu.scu.edu.cn>
2025-07-05 04:29:20 +0800

    QZone Done

--- src/Components/QZone.jsx
+++ src/Components/QZone.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-
+import swimming from "../assets/swimming.png"; // Adjust the path as necessary
+import Class from "../assets/class.png"; // Adjust the path as necessary
+import Playground from "../assets/playground.png"; // Adjust the path as necessary
 const QZone = () => {
-    return (
-        <div>
-            <h2 className='font-bold'>Q-Zone</h2>
-        </div>
-    );
+  return (
+    <div className="bg-gray-100 p-4 rounded-lg space-y-4 mt-4">
+      <h2 className="font-bold ">Q-Zone</h2>
+
+      <div className="flex flex-col items-center space-y-4">
+        <img src={swimming} alt="" />
+        <img src={Class} alt="" />
+        <img src={Playground} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default QZone;
